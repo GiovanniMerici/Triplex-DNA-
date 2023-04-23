25a9fecac448ecbc2083448e6f1241e1aab61190
Giovanni Merici <giovanni.merici@studenti.unipr.it>
2023-04-23 19:51:22 +0200

    Add files via upload

---
+++ grafico_per_spettro.m
@@ -0,0 +1,5 @@
+% import table
+% plot(name_of_table, 'Wavelength','Absorption')
+plot(spettrocopy, 'Wavelength','Absorption', Color='green')
+hold on
+plot(dnatriplextarget1, 'Wavelength','Absorption', Color='red')
